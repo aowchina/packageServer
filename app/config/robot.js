b2d6ba6837b8a469121a6075d07585d92eed554a
ericzhang <839513329@qq.com>
2017-09-01 19:53:14 +0800

    修改地址

--- app/config/robot.js
+++ app/config/robot.js
@@ -1,7 +1,7 @@
 
 var conf = {
-    // testUrl:"http://test.tongren.com:8090",//测试地址
-    testUrl:"http://localhost:8090",
+    testUrl:"http://test.tongren.com:8090",//测试地址
+    // testUrl:"http://localhost:8090",
     url:"https://server.tongren.com"//正式地址
 };
 
